20df467bde9417e7b0a5917bbe9d7d7350ec6c24
Demiurg <parserpro@gmail.com>
2020-01-18 20:41:11 +0300

    eject

--- src/home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Home extends React.Component {
-    render() {
-        return (
-            <div><h1>Home component</h1></div>
-        );
-    }
-};
